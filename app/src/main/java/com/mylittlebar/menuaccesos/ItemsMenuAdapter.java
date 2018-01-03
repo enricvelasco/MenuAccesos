bee67da0c935949db4ae0a5d6a8118db281baafb
thetallahassee <enric.velasco.g@gmail.com>
2018-01-03 22:12:37 +0100

    Initial commit

---
+++ app/src/main/java/com/mylittlebar/menuaccesos/ItemsMenuAdapter.java
@@ -0,0 +1,28 @@
+package com.mylittlebar.menuaccesos;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by enric on 3/1/18.
+ */
+
+public class ItemsMenuAdapter extends BaseAdapter{
+    private Context mContext;
+
+    // Array de identificadores
+    private Integer[] mThumbIds = {
+            /*R.drawable.e1,R.drawable.e2,R.drawable.e3,
+            R.drawable.e4,R.drawable.e5, R.drawable.e6,
+            R.drawable.e7,R.drawable.e8, R.drawable.e9,
+            R.drawable.e10,R.drawable.e11, R.drawable.e12,
+            R.drawable.e13,R.drawable.e14, R.drawable.e15,
+            R.drawable.e1,R.drawable.e2, R.drawable.e3,
+            R.drawable.e4,R.drawable.e5, R.drawable.e6,
+            R.drawable.e7,R.drawable.e8, R.drawable.e9*/
+    };
+
+    
+}
